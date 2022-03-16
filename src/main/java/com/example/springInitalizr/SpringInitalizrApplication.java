cf200886e618ba7b6e0fa31100e8769b7e11816b
michaelkirillov <kirilov.mikhail91@yandex.ru>
2022-03-16 19:27:22 +0300

    1st

---
+++ src/main/java/com/example/springInitalizr/SpringInitalizrApplication.java
@@ -0,0 +1,13 @@
+package com.example.springInitalizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitalizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitalizrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springInitalizr/SpringInitalizrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springInitalizr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitalizrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
